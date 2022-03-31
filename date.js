347e0cb6be0571261c568535dd64840d8261831c
njugunapm233 <njugunapm23@gmail.com>
2022-03-31 21:58:50 +0300

    First commit

--- date.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const dateTime = document.querySelector(".date");
-
-const nowTime = new Date();
-dateTime.textContent = `${nowTime}`;
-
-////////////////////////////////////////////////////////////////////////////////////////
